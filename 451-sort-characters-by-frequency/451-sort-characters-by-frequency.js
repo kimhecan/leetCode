cdcf14471e60366cdbde8239e21fa96c1ec09823
kimheechan <kimhecan@rainist.com>
2022-02-15 13:49:29 +0900

    Time: 133 ms (43.35%), Space: 50.1 MB (20.16%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
@@ -3,12 +3,17 @@
  * @return {string}
  */
 var frequencySort = function(s) {
-    const unsortedMap = s.split('').reduce((acc, cur) => {
-        return acc.get(cur) ? acc.set(cur, acc.get(cur) + 1) : acc.set(cur, 1);
+    
+    // Map객체 생성
+   const unsortedMap = s.split('').reduce((acc, cur) => {
+       return acc.get(cur) ? acc.set(cur, acc.get(cur) + 1) : acc.set(cur, 1);
     }, new Map());
   
+  
+  // array prototype 빌려서 sort로 많은 순 정렬
   const sortedMap = new Map([...unsortedMap].sort((a, b) => b[1] - a[1]));
   
+  // spread로 push
   const resultList = []
   sortedMap.forEach((value, key) => {
     resultList.push(...new Array(value).fill(key));
