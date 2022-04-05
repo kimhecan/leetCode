ad00043dc7e2e2a04f5586a92f7ef0d9ae64d9b4
kimheechan <kimhecan@rainist.com>
2022-04-05 13:09:01 +0900

    Time: 164 ms (24.24%), Space: 49.9 MB (5.19%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
@@ -0,0 +1,19 @@
+var findTheWinner = function(n, k) {
+  // [1,2,3,4,5,6]
+  let array = new Array(n).fill(0).map((v, i) => i+1);
+  let standingNumber = 1; 
+  while(array.length > 1) {
+    let lastLength = array.length;
+    const standingIndex = array.indexOf(standingNumber);
+    const removeIndex = (standingIndex + (k -1)) % lastLength
+    console.log(standingIndex, 'standingIndex')
+    array.splice(removeIndex, 1);
+    if(removeIndex === array.length) {
+    	standingNumber = array[0];  
+    } else {
+      console.log(removeIndex, array.length - 1)
+    	standingNumber = array[removeIndex];  
+    }
+  }
+  return array[0];
+};
